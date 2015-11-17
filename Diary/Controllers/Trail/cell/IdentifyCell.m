1ee142bd79f96248731d6d22762d9c30dcdba483
我 <2439398156@qq.com>
2015-11-17 17:33:21 +0800

    add identifyController

---
+++ Diary/Controllers/Trail/cell/IdentifyCell.m
@@ -0,0 +1,23 @@
+//
+//  IdentifyCell.m
+//  Diary
+//
+//  Created by 我 on 15/11/17.
+//  Copyright © 2015年 Owen. All rights reserved.
+//
+
+#import "IdentifyCell.h"
+
+@implementation IdentifyCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Diary/IdentifyController.h
@@ -0,0 +1,13 @@
+//
+//  IdentifyController.h
+//  Diary
+//
+//  Created by 我 on 15/11/17.
+//  Copyright © 2015年 Owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IdentifyController : UIViewController
+
+@end
