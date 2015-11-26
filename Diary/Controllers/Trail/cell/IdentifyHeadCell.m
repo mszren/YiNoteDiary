a3facdbeac78d488f855339558b0f97e0ac83983
我 <2439398156@qq.com>
2015-11-26 10:46:10 +0800

    Add Arsegmentcontroller

---
+++ Diary/Controllers/Trail/cell/IdentifyHeadCell.m
@@ -0,0 +1,23 @@
+//
+//  IdentifyHeadCell.m
+//  Diary
+//
+//  Created by 我 on 15/11/25.
+//  Copyright © 2015年 Owen. All rights reserved.
+//
+
+#import "IdentifyHeadCell.h"
+
+@implementation IdentifyHeadCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
